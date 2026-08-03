89465897af99a1242d50cc83a0e78d83fc1ea475
Amaru Daicz <amarudaicz10gmail.com>
2026-08-03 09:04:50 -0300

    tymes order status

--- services/OrderStatusService.php
+++ services/OrderStatusService.php
@@ -11,7 +11,7 @@ class OrderStatusService
     /**
      * Horarios en los que se permite ejecutar la actualización de estados.
      */
-    const RUN_TIMES = ['06:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00'];
+    const RUN_TIMES = ['06:00', '07:00','08:00','09:00','10:00','11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00'];
 
     /**
      * Directorio donde se depositan los pedidos listos para el sistema externo.
