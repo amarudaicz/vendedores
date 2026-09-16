f1de38592b5fcff110250d20d2749d8b57f29644
Amaru Daicz <amarudaicz10gmail.com>
2026-09-16 11:38:34 -0300

    chore: bump version to 1.0.9

--- public/pages/sellers/app/src/app/version.ts
+++ public/pages/sellers/app/src/app/version.ts
@@ -1,8 +1,8 @@
 // AUTO-GENERADO — no editar manualmente
-// Generado el 2026-09-16T14:23:25.112Z
+// Generado el 2026-09-16T14:40:26.857Z
 export const VERSION = {
-  version: '1.0.8',
+  version: '1.0.114',
   branch: 'nvd',
-  commit: '18236e2',
+  commit: '1a2ca9f',
   date: '2026-09-16',
 };
